aa79089cec76b210f26dcfac7222b2b6c5563ced
王天禹 <wandouba467@gmail.com>
2017-06-01 12:58:26 +0800

    write seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,92 @@ u.password_confirmation = "123456"   # 最少要六码
 u.is_admin = true
 u.save
 
-Product.create!(title: "1",
-                 description: "1",
-                 price: 1,
-                 quantity: 1,
-                 image: open("https://ws3.sinaimg.cn/large/006tNc79ly1fftx4k69qtj318y0u0u0x.jpg")
-                 )
+# Product.create!(title: "1",
+#                  description: "1",
+#                  price: 1,
+#                  quantity: 1,
+#                  image: open("https://ws3.sinaimg.cn/large/006tNc79ly1fftx4k69qtj318y0u0u0x.jpg")
+#                  )
+#
+# Product.create!(title: "2",
+#                 description: "2",
+#                 price: 2,
+#                 quantity: 2,
+#                 image: open("https://ws3.sinaimg.cn/large/006tNc79ly1fftx6n69exj31900u0kjl.jpg")
+#                 )
 
-Product.create!(title: "2",
-                description: "2",
-                price: 2,
-                quantity: 2,
-                image: open("https://ws3.sinaimg.cn/large/006tNc79ly1fftx6n69exj31900u0kjl.jpg")
+Product.create!(title: "爆款牛仔服",
+                 description: "修身",
+                 price: 328,
+                 quantity: 20,
+                 image: open("https://ws3.sinaimg.cn/large/006tNbRwly1fg4k8frb33j30by0bydh1.jpg")
+                 )
+Product.create!(title: "牛仔半袖",
+                description: "蓝色纯棉",
+                price: 128,
+                quantity: 50,
+                image: open("https://ws4.sinaimg.cn/large/006tNbRwly1fg4jf218j9j30by0byq45.jpg")
                 )
+Product.create!(title: "牛仔短裤",
+                 description: "清凉休闲",
+                 price: 199,
+                 quantity: 20,
+                 image: open("https://ws2.sinaimg.cn/large/006tNbRwly1fg4jaor4lnj30by0bymyb.jpg")
+                 )
+Product.create!(title: "百搭鞋",
+                description: "舒适透气",
+                price: 299,
+                quantity: 50,
+                image: open("https://ws4.sinaimg.cn/large/006tNbRwly1fg4jaogq0ej30by0byjsg.jpg")
+                )
+
+Product.create!(title: "夏季防风运动衣",
+               description: "透气",
+               price: 199,
+               quantity: 50,
+               image: open("https://ws3.sinaimg.cn/large/006tNbRwly1fg4jm8mp30j30by0byt9r.jpg")
+               )
+Product.create!(title: "室内多功能运动器械",
+              description: "引体向上、仰卧起坐",
+              price: 1288,
+              quantity: 10,
+              image: open("https://ws3.sinaimg.cn/large/006tNbRwly1fg4jm8yg2kj30by0bydgd.jpg")
+              )
+Product.create!(title: "nike休闲跑鞋",
+               description: "舒适透气",
+               price: 799,
+               quantity: 100,
+               image: open("https://ws3.sinaimg.cn/large/006tNbRwly1fg4jm8riw1j30by0byjrq.jpg")
+               )
+Product.create!(title: "跑步健身套装",
+              description: "舒适透气",
+              price: 388,
+              quantity: 5,
+              image: open("https://ws1.sinaimg.cn/large/006tNbRwly1fg4jm92s8bj30by0byt8o.jpg")
+              )
+
+
+Product.create!(title: "商务西服套装",
+               description: "优雅帅气",
+               price: 688,
+               quantity: 10,
+               image: open("https://ws3.sinaimg.cn/large/006tNbRwly1fg4ju871psj30by0bymxs.jpg")
+               )
+Product.create!(title: "燕尾服套装",
+              description: "结婚、指挥、宴会专用",
+              price: 799,
+              quantity: 5,
+              image: open("https://ws3.sinaimg.cn/large/006tNbRwly1fg4ju825bej30by0byaal.jpg")
+              )
+Product.create!(title: "夏季衬衣",
+               description: "免烫纯棉",
+               price: 288,
+               quantity: 30,
+               image: open("https://ws1.sinaimg.cn/large/006tNbRwly1fg4ju7wejaj30by0by0t9.jpg")
+               )
+Product.create!(title: "花花公子商务皮鞋",
+              description: "舒适透气",
+              price: 688,
+              quantity: 15,
+              image: open("https://ws4.sinaimg.cn/large/006tNbRwly1fg4ju7lhk1j30by0byaac.jpg")
+              )
