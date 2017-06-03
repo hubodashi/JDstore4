994063fcd987378ca790d0c0ce2270d0060bc75b
忽波 <hubo@hubodeMacBook-Pro.local>
2017-06-03 14:59:55 +0800

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,130 +25,130 @@ Product.create!(title: "爆款牛仔服",
                  description: "修身",
                  price: 328,
                  quantity: 20,
-                 image: open("/images/006tNbRwly1fg4k8frb33j30by0bydh1.jpg")
+                 image_path: "/images/006tNbRwly1fg4k8frb33j30by0bydh1.jpg"
                  )
 
 Product.create!(title: "牛仔半袖",
                 description: "蓝色纯棉",
                 price: 128,
                 quantity: 50,
-                image: open("/images/006tNbRwly1fg4jf218j9j30by0byq45.jpg")
+                 image_path: "/images/006tNbRwly1fg4jf218j9j30by0byq45.jpg"
                 )
 
 Product.create!(title: "牛仔短裤",
                  description: "清凉休闲",
                  price: 199,
                  quantity: 20,
-                 image: open("/images/006tNbRwly1fg4jaor4lnj30by0bymyb.jpg")
+                 image_path: "/images/006tNbRwly1fg4jaor4lnj30by0bymyb.jpg"
                  )
 
 Product.create!(title: "百搭鞋",
                 description: "舒适透气",
                 price: 299,
                 quantity: 50,
-                image: open("/images/006tNbRwly1fg4jaogq0ej30by0byjsg.jpg")
+                image_path: "/images/006tNbRwly1fg4jaogq0ej30by0byjsg.jpg"
                 )
 
 Product.create!(title: "夏季防风运动衣",
                description: "透气",
                price: 199,
                quantity: 50,
-               image: open("/images/006tNbRwly1fg4jm8mp30j30by0byt9r.jpg")
+               image_path: "/images/006tNbRwly1fg4jm8mp30j30by0byt9r.jpg"
                )
 
 Product.create!(title: "室内多功能运动器械",
               description: "引体向上、仰卧起坐",
               price: 1288,
               quantity: 10,
-              image: open("/images/006tNbRwly1fg4jm8yg2kj30by0bydgd.jpg")
+              image_path: "/images/006tNbRwly1fg4jm8yg2kj30by0bydgd.jpg"
               )
 
 Product.create!(title: "nike休闲跑鞋",
                description: "舒适透气",
                price: 799,
                quantity: 100,
-               image: open("/images/006tNbRwly1fg4jm8riw1j30by0byjrq.jpg")
+               image_path: "/images/006tNbRwly1fg4jm8riw1j30by0byjrq.jpg"
                )
 
 Product.create!(title: "跑步健身套装",
               description: "舒适透气",
               price: 388,
               quantity: 5,
-              image: open("/images/006tNbRwly1fg4jm92s8bj30by0byt8o.jpg")
+              image_path: "/images/006tNbRwly1fg4jm92s8bj30by0byt8o.jpg"
               )
 
 Product.create!(title: "商务西服套装",
                description: "优雅帅气",
                price: 688,
                quantity: 10,
-               image: open("/images/006tNbRwly1fg4ju871psj30by0bymxs.jpg")
+               image_path: "/images/006tNbRwly1fg4ju871psj30by0bymxs.jpg"
                )
 
 Product.create!(title: "燕尾服套装",
               description: "结婚、指挥、宴会专用",
               price: 799,
               quantity: 5,
-              image: open("/images/006tNbRwly1fg4ju825bej30by0byaal.jpg")
+              image_path: "/images/006tNbRwly1fg4ju825bej30by0byaal.jpg"
               )
 
 Product.create!(title: "夏季衬衣",
                description: "免烫纯棉",
                price: 288,
                quantity: 30,
-               image: open("/images/006tNbRwly1fg4ju7wejaj30by0by0t9.jpg")
+               image_path: "/images/006tNbRwly1fg4ju7wejaj30by0by0t9.jpg"
                )
 
 Product.create!(title: "花花公子商务皮鞋",
               description: "舒适透气",
               price: 688,
               quantity: 15,
-              image: open("/images/006tNbRwly1fg4ju7lhk1j30by0byaac.jpg")
+              image_path: "/images/006tNbRwly1fg4ju7lhk1j30by0byaac.jpg"
               )
 
 Photo.create!(product_id:1,
-              avatar: open("/images/006tNbRwly1fg4k8frb33j30by0bydh1.jpg")
+              avatar_path: "/images/006tNbRwly1fg4k8frb33j30by0bydh1.jpg"
               )
 
 Photo.create!(product_id:2,
-              avatar: open("/images/006tNbRwly1fg4jf218j9j30by0byq45.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jf218j9j30by0byq45.jpg"
               )
 
 Photo.create!(product_id:3,
-              avatar: open("/images/006tNbRwly1fg4jaor4lnj30by0bymyb.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jaor4lnj30by0bymyb.jpg"
               )
 
 Photo.create!(product_id:4,
-              avatar: open("/images/006tNbRwly1fg4jaogq0ej30by0byjsg.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jaogq0ej30by0byjsg.jpg"
               )
 
 Photo.create!(product_id:5,
-              avatar: open("/images/006tNbRwly1fg4jm8mp30j30by0byt9r.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jm8mp30j30by0byt9r.jpg"
               )
 
 Photo.create!(product_id:6,
-              avatar: open("/images/006tNbRwly1fg4jm8yg2kj30by0bydgd.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jm8yg2kj30by0bydgd.jpg"
               )
 
 Photo.create!(product_id:7,
-              avatar: open("/images/006tNbRwly1fg4jm8riw1j30by0byjrq.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jm8riw1j30by0byjrq.jpg"
               )
 
 Photo.create!(product_id:8,
-              avatar: open("/images/006tNbRwly1fg4jm92s8bj30by0byt8o.jpg")
+              avatar_path: "/images/006tNbRwly1fg4jm92s8bj30by0byt8o.jpg"
               )
 
 Photo.create!(product_id:9,
-              avatar: open("/images/006tNbRwly1fg4ju871psj30by0bymxs.jpg")
+              avatar_path: "/images/006tNbRwly1fg4ju871psj30by0bymxs.jpg"
               )
 
 Photo.create!(product_id:10,
-              avatar: open("/images/006tNbRwly1fg4ju825bej30by0byaal.jpg")
+              avatar_path: "/images/006tNbRwly1fg4ju825bej30by0byaal.jpg"
               )
 
 Photo.create!(product_id:11,
-              avatar: open("/images/006tNbRwly1fg4ju7wejaj30by0by0t9.jpg")
+              avatar_path: "/images/006tNbRwly1fg4ju7wejaj30by0by0t9.jpg"
               )
 
 Photo.create!(product_id:12,
-              avatar: open("/images/006tNbRwly1fg4ju7lhk1j30by0byaac.jpg")
+              avatar_path: "/images/006tNbRwly1fg4ju7lhk1j30by0byaac.jpg"
               )
