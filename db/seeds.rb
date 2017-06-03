f0276d663a999b2fd9b208fb424cf80c0011f645
忽波 <hubo@hubodeMacBook-Pro.local>
2017-06-03 10:15:29 +0800

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
+
 u.email = "123@123.com"           # 可以改成自己的 email
 
 u.password = "123456"                # 最少要六码
@@ -13,6 +14,7 @@ u.password = "123456"                # 最少要六码
 u.password_confirmation = "123456"   # 最少要六码
 
 u.is_admin = true
+
 u.save
 
 Category.create(name: "牛仔范")
